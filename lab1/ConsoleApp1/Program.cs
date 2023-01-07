a4533488baa33de0a9796f15bfcd1a95948735d9
reteylouq <90748583+reteylouq@users.noreply.github.com>
2023-01-07 19:41:17 +0200

    lab1

---
+++ lab1/ConsoleApp1/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace lab1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double h, R, V;
+            Console.WriteLine("Введiть висоту ");
+            h = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введiть радіуc");
+            R = double.Parse(Console.ReadLine());
+            if (R > 0)
+            {
+                Console.WriteLine("V = h*R^2*PI");
+
+                V = (Math.Pow(R, 2)) * h * Math.PI;
+                // V = (Math.Pow(R, 2)) * h * P; ne rabotaet(
+                Console.WriteLine(V);
+            }
+            else
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
