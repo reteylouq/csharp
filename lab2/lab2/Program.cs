798b28a5c2045be60fbe794a1a8da1bb359469a8
reteylouq <90748583+reteylouq@users.noreply.github.com>
2023-01-07 19:59:26 +0200

    lab2

---
+++ lab2/lab2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace lab2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int nn, nk, k;
+            double res = 0;
+            nn = int.Parse(Console.ReadLine());
+            nk = int.Parse(Console.ReadLine());
+
+            for (k = nn; k < nk; k++)
+            {
+                if (0 <= nn & nn <= nk)
+                {
+                   res += (Math.Pow(-1, (Math.Pow(k, 2)) + 1) * Math.Pow(k, 2) - 2) / (3 * Math.Pow(k, 2) - 2 * k);
+                }
+                else
+                {
+                    Console.WriteLine("error");
+                    return;
+                }
+            }
+            Console.WriteLine(res);
+        }
+    }
+}
